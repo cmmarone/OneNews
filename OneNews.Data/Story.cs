27c6f2b9b9570213115ce0ddb1315248e287f660
cmmarone <cmmarone@gmail.com>
2021-04-20 14:13:52 -0400

    addedRequiredAttributesToStoryEntity

--- OneNews.Data/Story.cs
+++ OneNews.Data/Story.cs
@@ -13,18 +13,28 @@ namespace OneNews.Data
 		[Key]
 		public int Id { get; set; }
 
+		[Required]
 		[ForeignKey(nameof(Category))]
 		public int CategoryId { get; set; }
 		public virtual Category Category { get; set; }
 
+		[Required]
 		[ForeignKey(nameof(Writer))]
 		public int WriterId { get; set; }
 		public virtual Writer Writer { get; set; }
 
+		[Required]
 		public string Title { get; set; }
+
+		[Required]
 		public string Body { get; set; }
+
+		[Required]
 		public string Location { get; set; }
+
+		[Required]
 		public DateTimeOffset TimeOfPublication { get; set; }
+
 		public Guid AuthorId { get; set; }
 	}
 }
