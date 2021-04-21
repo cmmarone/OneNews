82f1bca15c5b0da5235bba28659e702d690c7d1a
vscott1989 <vscott1811@gmail.com>
2021-04-21 14:53:56 -0400

    UpdatedController

--- OneNews.WebAPI/Controllers/CategoryController.cs
+++ OneNews.WebAPI/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OneNews.Models;
+using OneNews.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,64 @@ namespace OneNews.WebAPI.Controllers
 {
     public class CategoryController : ApiController
     {
+        private CategoryService CreateCategoryService()
+        {
+            var categotyService = new CategoryService();
+            return categotyService;
+        }
+
+        public IHttpActionResult Post(CategoryCreate model)
+        {
+            if (model is null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var category = CreateCategoryService();
+            bool isSuccessful = category.CreateCategory(model);
+            if (isSuccessful)
+            {
+                return Ok($"Category Created:{model.Name}");
+            }
+            return InternalServerError();
+        }
+
+        public IHttpActionResult Get()
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var categories = categoryService.GetCategories();
+            return Ok(categories);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var category = categoryService.GetCategorybyId(id);
+            return Ok(category);
+        }
+
+        public IHttpActionResult Put(CategoryEdit category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var categoryService = CreateCategoryService();
+            if (!categoryService.UpdateCategory(category))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var categoryService = CreateCategoryService();
+
+            if (!categoryService.DeleteCategory(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
--- OneNews.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ OneNews.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
+using OneNews.Data;
 using OneNews.WebAPI.Models;
 
 namespace OneNews.WebAPI.Providers
